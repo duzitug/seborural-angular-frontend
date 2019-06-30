99dec83a6049a0fd24d3072f7ba3b38d56e7854b
merciof <mercio.andrade@ufrpe.br>
2019-06-30 18:30:21 -0300

    Adição do componente create-book.

--- sebo-rural/src/app/app.module.ts
+++ sebo-rural/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './create-user/create-user.component';
+import { CreateBookComponent } from './create-book/create-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateUserComponent
+    CreateUserComponent,
+    CreateBookComponent
   ],
   imports: [
     BrowserModule,
