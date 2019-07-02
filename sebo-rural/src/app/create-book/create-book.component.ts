68c68985e979d08ca4324e7e3663a7e6c2daf271
merciof <mercio.andrade@ufrpe.br>
2019-07-02 13:56:51 -0300

    Adição de função createBook() no componente create-book.

--- sebo-rural/src/app/create-book/create-book.component.ts
+++ sebo-rural/src/app/create-book/create-book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-book',
@@ -15,9 +16,28 @@ export class CreateBookComponent implements OnInit {
   descricao: string;
   urlFoto: string;
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+    // titulo: string,
+    // autor: string,
+    // curso: string,
+    // periodo: number,
+    // disciplina: string,
+    // descricao: string,
+    // urlFoto: string
+    
+    ) { }
 
   ngOnInit() {
+  }
+
+  createBook () {
+    window.console.log(this.http.post('http://localhost:8080/book', {
+      titulo: this.titulo,
+      autor: this.autor
+    }).subscribe(
+      response => window.console.log(response)
+    ));
   }
 
 }
