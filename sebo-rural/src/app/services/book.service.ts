2903b2f67934c4ae50efb5127ce1c895816e5b7a
merciof <mercio.andrade@ufrpe.br>
2020-05-26 21:23:16 -0300

    adicao de objeto httpHeaders ao book.service

--- sebo-rural/src/app/services/book.service.ts
+++ sebo-rural/src/app/services/book.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpHeaders  } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { Book } from '../models/book.model';
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+	http: HttpClient;
+	httpHeaders: HttpHeaders;
+
+  constructor(http: HttpClient, httpHeaders: HttpHeaders) {
+  	this.http = http;
+  	//this.httpHeader = httpHeader;
+  	this.httpHeaders = httpHeaders.set('Authorization', localStorage.getItem('access_token'));
+   }
+
+
+   getBookById(id: number) : Observable<Book> {
+   	
+   	return this.http.get<Book>('https://sebo-rural.herokuapp.com/api' + id);
+   
+   }
+
+   
+
+
+
+
+
+
+
+
+
 }
