7e77d9d59b193e83931d0a5352ba04a333c8f25a
merciof <mercio.andrade@ufrpe.br>
2021-07-03 10:46:04 -0300

    add navbar component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sebo-rural-navbar',
+  template: '<p>navbar works!</p>'
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
