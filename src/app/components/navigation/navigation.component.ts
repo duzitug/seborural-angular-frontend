4a65675f0978d65aeabfc276aafaecbc8b3d9ca0
merciof <mercio.andrade@ufrpe.br>
2021-07-03 16:19:49 -0300

    add navigation material component

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'sebo-rural-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent implements OnInit {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+
+  }
+
+  listarCursos(): void {
+    alert('listar cursos clicado!');
+  }
+}
