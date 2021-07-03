5c370f2979d15880dc6d206586b202c13c422677
merciof <mercio.andrade@ufrpe.br>
2021-07-03 10:46:31 -0300

    add main content component

---
+++ src/app/components/main-content/main-content.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sebo-rural-main-content',
+  template: `
+    <p>
+      main-content works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MainContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
