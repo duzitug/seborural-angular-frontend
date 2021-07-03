743c88ea39a695162912ff0770c866f8a8712c72
merciof <mercio.andrade@ufrpe.br>
2021-07-03 10:46:20 -0300

    add footer component

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sebo-rural-footer',
+  template: '<p>footer works!</p>'
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
