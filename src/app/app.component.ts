122e499306f66301fb61849a173aa9b9af9df62b
merciof <mercio.andrade@ufrpe.br>
2021-07-11 12:35:42 -0300

    edit template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {CourseService} from './services/course.service';
-import {Course} from './models/course.model';
-import {Genre} from './models/genre.model';
-import {GenreService} from './services/genre.service';
-import {StudentService} from './services/student.service';
-import {BookService} from './services/book.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  template: `<sebo-rural-navigation></sebo-rural-navigation>`
 })
-export class AppComponent implements OnInit {
-  title = 'angular11-route';
-  courses: Course[] = [];
+export class AppComponent {
 
-  genres: Genre[] = [];
-
-  constructor(private courseService: CourseService,
-              private genreService: GenreService,
-              private studentService: StudentService,
-              private bookService: BookService)
-              {}
-
-  ngOnInit(): void {
-
-    this.courseService.getAllCourses()
-      .subscribe(
-        response => { this.courses = response;  window.console.log(this.courses);    }
-      );
-
-    this.genreService.getAllGenres()
-      .subscribe(resposta => window.console.log(resposta) );
-
-    this.studentService.getAllStudents()
-      .subscribe(resposta => window.console.log(resposta) );
-
-    this.bookService.getAllBooks()
-      .subscribe(resposta => window.console.log(resposta) );
-
-    this.studentService.getStudentById(2)
-      .subscribe(resposta => window.console.log(resposta) );
-
-
-  }
-
-
-
+  constructor() {}
 
 }
