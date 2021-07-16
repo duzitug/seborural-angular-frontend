71fe4ebe6b1fbc23d1de1c1c044fc27cbf946d27
merciof <mercio.andrade@ufrpe.br>
2021-07-16 17:42:27 -0300

    add book-list component

--- angular/src/app/components/book/book-list.component.ts
+++ angular/src/app/components/book/book-list.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from "rxjs";
-import { Book } from "../../models/book.model";
-import { BookService } from "../../services/book.service";
+import { Observable } from 'rxjs';
+import { Book } from '../../models/book.model';
+import { BookService } from '../../services/book.service';
 
 @Component({
   selector: 'sebo-rural-book',
-  templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
 })
-export class BookComponent implements OnInit {
+export class BookListComponent implements OnInit {
 
   books$: Observable<Book[]> = new Observable<Book[]>();
 
