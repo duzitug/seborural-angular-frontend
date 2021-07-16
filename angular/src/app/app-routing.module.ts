5de8f4720cfde0a0f27833108d23ff29d1ffde99
merciof <mercio.andrade@ufrpe.br>
2021-07-16 17:46:40 -0300

    add BookListByCourseId route

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { FooterComponent } from './components/footer/footer.component';
-import { CourseComponent } from "./components/course/course.component";
-import { BookComponent } from "./components/book/book.component";
-import { GenreComponent } from "./components/genre/genre.component";
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { CourseComponent } from './components/course/course.component';
+import { BookListComponent } from './components/book/book-list.component';
+import { GenreComponent } from './components/genre/genre.component';
+import { BookListByCourseIdComponent } from './components/book/book-list-by-course-id/book-list-by-course-id.component';
 
 const routes: Routes = [
-   {path: '', component: BookComponent},
+   {path: '', component: BookListComponent},
    {path: 'cursos', component: CourseComponent},
-   {path: 'livros', component: BookComponent},
+   {path: 'livros', component: BookListComponent},
+   {path: 'livros/:courseId', component: BookListByCourseIdComponent},
    {path: 'generos', component: GenreComponent},
    {path: 'footer', component: FooterComponent}
 ];
